97b31935b80c8818c570fb339c04c597300b8cb9
SEPL\Priyanka.mali <priyankamali522@gmail.com>
2018-07-10 19:04:10 +0530

    Swap Number

---
+++ src/swap_2_number.java
@@ -0,0 +1,16 @@
+public class swap_2_number {
+
+    public static void main (String [] args){
+
+        int num1 = 100;
+        int num2 = 50;
+
+        num1 =num1+num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("num1 is" + num1);
+        System.out.println("num2 is" + num2);
+
+    }
+}
